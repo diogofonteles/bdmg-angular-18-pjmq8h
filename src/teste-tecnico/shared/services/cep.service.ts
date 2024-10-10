3e311fab46a39d772e10eca1bd878dd92bb4084d
Diogo Fonteles <diogo.fonteles@satcom-int.com>
2024-10-10 12:10:31 -0300

    chore: add getEndereco service

---
+++ src/teste-tecnico/shared/services/cep.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import {EnderecoResponse} from "../models/responses/endereco.response";
+
+@Injectable({
+    providedIn: 'root',
+})
+export class CepService {
+    constructor(private httpClient: HttpClient) {}
+
+    getEndereco(cep: string): Observable<EnderecoResponse> {
+        return this.httpClient.get<EnderecoResponse>(
+            `https://viacep.com.br/ws/${cep}/json/`
+        );
+    }
+}
