a395a00ab286671143896c3445d2fdfb4fa9f72c
Diogo Fonteles <diogo.fonteles@satcom-int.com>
2024-10-10 12:05:55 -0300

    chore: add endereco response model

---
+++ src/teste-tecnico/shared/models/responses/endereco.response.ts
@@ -0,0 +1,14 @@
+export interface EnderecoResponse {
+    cep: string;
+    logradouro: string;
+    complemento: string;
+    bairro: string;
+    localidade: string;
+    uf: string;
+    estado: string;
+    regiao: string;
+    ibge: string;
+    gia: string;
+    ddd: string;
+    siafi: string;
+}
